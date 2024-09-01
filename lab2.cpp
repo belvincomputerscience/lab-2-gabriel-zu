4e656666f667fc694babfec17a668d5ec5364251
gabriel-zu <zuraagabriel@gmail.com>
2024-09-01 04:03:27 +0000

    Lab #2 Gabriel Zuraa

--- lab2.cpp
+++ lab2.cpp
@@ -1,1 +1,30 @@
+// This code calculates the average grade. Gabriel Zuraa. Lab #2
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int grade1;
+    int grade2;
+    int grade3;
+    int grade4;
+    float avg_grade;
+    
+    cout << "First grade?" << endl;
+    cin >> grade1;
+    
+    cout << "Second grade?" << endl;
+    cin >> grade2;
+    
+    cout << "Third grade?" << endl;
+    cin >> grade3;
+    
+    cout << "Fourth grade?" << endl;
+    cin >> grade4;
+    
+    avg_grade = (grade1 + grade2 + grade3 + grade4) / 4;
+    cout << "Average grade is: " << avg_grade << endl;
+
+    return 0;
+}
